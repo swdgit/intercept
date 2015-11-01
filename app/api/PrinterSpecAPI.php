1328ef8f1f1ed2e4fffdb0fe17ef35d4893cfd6f
Stacy Decker <stacy.decker@gmail.com>
2015-10-31 20:38:36 -0600

    printer spec insert or update added in. basic web-site template added in. Need to make it unique for the site.

--- app/api/PrinterSpecAPI.php
+++ app/api/PrinterSpecAPI.php
@@ -2,7 +2,27 @@
 class PrinterSpecAPI {
     
     public static function setPrinterSpec($params) {
-        echo '';
+        $printSpec = new PrintSpec();
+
+        $printSpec->printerMaterialXrefId = $params['printerMaterialXrefId'];
+        $printSpec->title                 = $params['title'];
+        $printSpec->description           = $params['description'];
+        $printSpec->profile               = $params['profile'];
+        $printSpec->recommendations       = $params['recommendations'];
+        
+        // new printspec in this case
+        if ($params['print_spec_id'] == -1) {
+            
+            $printSpec->printSpecId = DAOFactory::getPrintSpecDAO()->insert($printSpec);
+            
+        } else {
+            // update a print spec
+            $printSpec->printSpecId = $params['printSpecId'];
+
+            DAOFactory::getPrintSpecDAO()->update($printSpec);
+        }
+        
+        return $printSpec;
     }
 }
 ?>
