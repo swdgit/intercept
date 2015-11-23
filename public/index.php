b10b5da0bc4fc154e4bc1bc4865c212ba792857f
Stacy Decker <stacy.decker@gmail.com>
2015-11-23 00:46:34 -0700

    adding in calls for printers and materials

--- public/index.php
+++ public/index.php
@@ -1,47 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Proto-Lounge Printer and Material Intercept Database</title>
+<title>3D Print Specs</title>
 <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
 <meta name="description" content="3D Printer and Material intercection of usage and specs." />
 <meta name="keywords" content="3D Print Printing filament PLA ABS " />
 <meta name="robots" content="index, follow" />
 <!-- 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />  -->
 <link rel="stylesheet" type="text/css" href="public/css/screen.css" media="screen" />
-
-<script type="text/javascript">
-function getPrintersByMaterial() {
-    
-    var e = document.getElementById("materialSelect");
-    var materialId = e.options[e.selectedIndex].value;
-
-    alert("value : " + materialId);
-}
-
-function getMaterialByPrinter() {
-    var e = document.getElementById("printerSelect");
-    var printerId = e.options[e.selectedIndex].value;
-}
-</script>
 </head>
-<?php
-    $dir_iterator = new RecursiveDirectoryIterator ( "../app" );
-    
-    $iterator = new RecursiveIteratorIterator ( $dir_iterator, RecursiveIteratorIterator::SELF_FIRST );
-    // could use CHILD_FIRST if you so wish
-    
-    foreach ( $iterator as $file ) {
-        if ($file->isFile ()) {
-            require_once $file;
-        }
-    }
-    $intercept = new Intercept ();
-    $matrix = $intercept->getMatrix ();
-    
-    $printers = $intercept->getPrinters ();
-    
-    $materials = $intercept->getMaterials ();
-?>
 <body>
  <div id="header">
   <h1>The Perfect 3 Column Liquid Layout (Percentage widths)</h1>
@@ -59,11 +26,6 @@ function getMaterialByPrinter() {
      <h2>Materials</h2>
      <select id="materialSelect" onselect="getPrintersByMaterial();">
        <option value=""  >Select Your Material</option>
-        <?php
-            foreach ( $materials as $m ) {
-                echo '<option value="'. $m->materialId . '">' . $m->type . '</option>';
-            }
-        ?>
      </select>
 
      <!-- Column 1 end -->
@@ -73,11 +35,6 @@ function getMaterialByPrinter() {
      <h2>Printers</h2>
      <select id="printerSelect" onselect="getMaterialByPrinter();">
        <option value=""  >Select Your Printer</option>
-        <?php
-            foreach ( $printers as $p ) {
-                echo '<option value="'. $p->printerId . '">' . $p->name . '</option>';
-            }
-        ?>
        </select>
 
      <!-- Column 2 end -->
@@ -87,11 +44,6 @@ function getMaterialByPrinter() {
      <div id="ads"></div>
      <h2>Intersection</h2>
      <ul>
-    <?php
-        foreach ( $matrix as $m ) {
-            echo '<li>' . $m->printerName . '</li>';
-        }
-    ?>
      </ul>
      <!-- Column 3 end -->
     </div>
---
+++ public/js/init.js
@@ -0,0 +1,38 @@
+/**
+ * Use me to init the printers and materials on the site.
+ * 
+ *  
+ */
+
+
+function initPage() {
+    // get the printers
+    // get the materials
+    
+    //set the list, just the names, and the types to the 2 different list windows.
+    
+    $.getJSON( "api/get/suppliers/PRINTER", function( data ) {
+        var items = [];
+        $.each( data, function( key, val ) {
+          items.push( "<li id='" + key + "'>" + val + "</li>" );
+        });
+       
+        $( "<ul/>", {
+          "class": "printers",
+          html: items.join( "" )
+        }).appendTo( "printerList" );
+      });
+    
+    $.getJSON("api/get/suppliers/MATERIAL", function(data) {
+        var items = [];
+        $.each( data, function( key, val ) {
+          items.push( "<li id='" + key + "'>" + val + "</li>" );
+        });
+       
+        $( "<ul/>", {
+          "class": "materials",
+          html: items.join( "" )
+        }).appendTo( "materialList" );
+        
+    });
+}
