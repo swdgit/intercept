cc1d59537a343609d8a2dcb859e6efe37fdbe984
Stacy Decker <stacy.decker@gmail.com>
2015-11-24 08:51:21 -0700

    remote path is different.

--- public/api.php
+++ public/api.php
@@ -1,6 +1,6 @@
 <?php
-    require 'vendor/autoload.php';
-    require 'app/autoload.php';
+    require '../vendor/autoload.php';
+    require '../app/autoload.php';
 
     $app = new \Slim\Slim();
 
