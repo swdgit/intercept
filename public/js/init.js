9368cc5a163362170630ec97e26b0e10557df392
Stacy Decker <stacy.decker@gmail.com>
2015-11-23 22:51:08 -0700

    trying

--- public/js/init.js
+++ public/js/init.js
@@ -11,7 +11,7 @@ function initPage() {
     
     //set the list, just the names, and the types to the 2 different list windows.
     
-    $.get( "/api/get/suppliers/PRINTER", function( resp ) {
+    $.getJSON( "api/get/suppliers/PRINTER", function( resp ) {
         console.log( resp ); // server response
     });
     
